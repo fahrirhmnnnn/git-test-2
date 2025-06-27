bf067831d43e50de07e73237b16413f0c31680e0
fahrirhmnnnn <rahmanfahri457@gmail.com>
2025-06-27 17:30:14 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat odit, exercitationem eos ea, voluptatem quod ratione fugit natus laboriosam veniam sed quo ex? Perspiciatis et cupiditate ut, qui enim unde?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
